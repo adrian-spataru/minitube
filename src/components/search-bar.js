64ecc5b750b6233387136fcff61677d84cfb901b
Adrian Spataru <adrian.spataru@student.tugraz.at>
2016-02-19 19:42:39 +0100

    added youtube search

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -4,15 +4,22 @@ import ReactDOM from 'react-dom';
 class SearchBar extends Component {
     constructor(props){
         super(props);
-        this.state = { term: "" };
+        this.state = {term: '' };
 
+    }
+
+    eentHandler(event){
+
+        const a = event.target.value;
+        this.setState({term: a});
+        this.props.onQueryChange(this.state.term);
     }
 
     render(){
 
         return(
             <div className="ui fluid icon input">
-                <input type="text" placeholder="Search..." />
+                <input onChange={this.eentHandler} type="text" placeholder="Search..." />
                 <i className="search icon"></i>
             </div>
         );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search-bar';
+import YTSearch from 'youtube-api-search';
 
+const API_KEY = "AIzaSyBCvy6nZxJep2z93GVyK4ProQeAMQ9ClDk";
 
 
 class App extends Component{
     constructor(props){
         super(props);
+
+        this.state = { selectedVideo: null};
+        this.searchQuery("developers");//used as a default value
+    }
+
+    searchQuery(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({selectedVideo: videos[0]}); // We will display for the moment only the first video
+            console.log(videos[0].snippet.title); // Used for Debugging
+        });
     }
 
     render(){
@@ -20,7 +32,7 @@ class App extends Component{
                     </h1>
                 </div>
                 <div className="six wide column ">
-                    <SearchBar />
+                    <SearchBar onQueryChange={term => this.searchQuery(term)} />
                 </div>
             </div>
 
